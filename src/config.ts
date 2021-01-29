ee1943ea3f0a448b9b09a1c5568fe4af15dc9dea
fedyk <fedyk72@gmail.com>
2021-01-29 17:59:28 +0100

    Fix config

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 export const __DEV__ = location.hostname === "localhost"
 export const LIVECHAT_CLIENT_ID = "b71bb162a2aaebf04bdc4abcb91632ae"
-export const LIVEHCAT_REDIRECT_URL = "http://localhost:8080"
-export const AGENT_API_HOSTS: { [key: string]: string } = {
+export const LIVEHCAT_REDIRECT_URL = __DEV__ ? "http://localhost:8080" : "https://fedyk.github.io/livechat-x/"
+export const AGENT_API_HOSTS: Record<string, string> = {
   dal: "api.livechatinc.com",
   fra: "api-fra.livechatinc.com",
 }
 
-export function getAccountsUrl(path = "", state =  "") {
+export function getAccountsUrl(path = "", state = "") {
   return `https://accounts.livechat.com/${path}` +
     `?response_type=token` +
     `&client_id=${encodeURIComponent(LIVECHAT_CLIENT_ID)}` +
@@ -16,7 +16,7 @@ export function getAccountsUrl(path = "", state =  "") {
 
 export function getAgentAPIHost(region: string) {
   const host = AGENT_API_HOSTS[region]
-  
+
   if (!host) {
     throw new Error(`Unsupported region ${region}`)
   }
