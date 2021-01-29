b25d53345c9cd49fce6d6449ab798074f2d8b7cf
fedyk <fedyk72@gmail.com>
2021-01-29 18:12:36 +0100

    Update config

--- modules/config.js
+++ modules/config.js
@@ -1,6 +1,6 @@
 export const __DEV__ = location.hostname === "localhost";
 export const LIVECHAT_CLIENT_ID = "b71bb162a2aaebf04bdc4abcb91632ae";
-export const LIVEHCAT_REDIRECT_URL = "http://localhost:8080";
+export const LIVEHCAT_REDIRECT_URL = __DEV__ ? "http://localhost:8080" : "https://fedyk.github.io/livechat-x/";
 export const AGENT_API_HOSTS = {
     dal: "api.livechatinc.com",
     fra: "api-fra.livechatinc.com",
